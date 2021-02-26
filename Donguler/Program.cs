8c881209ea94885d682337aa45327e0d2648c085
Muhammet Emin Aslangil <eminaslangil@hotmail.com>
2021-02-26 13:09:51 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array - dizi
+
+            string[] kurslar = new string[]{ "Yazılım Geliştirici Yetiştirme Kampı",
+            "Programlamaya Başlamak İçin Temel Kurs","Java Kursu"};
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitt foreach başladı");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 20;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi + " \n"
+                    + urun.Aciklama + "\n "
+                    + urun.Fiyati
+                    + "\n --------------- \n");
+
+            }
+
+            Console.WriteLine("-----------Metotlar-----------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun2);
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+
+
+            Console.ReadKey();
+        }
+    }
+}
