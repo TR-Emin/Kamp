c77afb3869b4b63fab8242bc3ab84d8b76801d4b
Muhammet Emin Aslangil <eminaslangil@hotmail.com>
2021-02-28 14:18:37 +0300

    28 Şubat ders sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        } 
+
+        public int Topla(params int[] sayilar)
+        {
+            int toplam = 0;
+            foreach (int sayi in sayilar)
+            {
+                toplam += sayi;
+            }
+            return toplam;
+        }
+        
+    }
+}
